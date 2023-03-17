f3374289f64938de6b9ad74795dc54a80721b55e
Omprakash <omprakash8578502@gmail.com>
2023-03-17 20:42:58 +0530

    UpdateApp is added

---
+++ 01-JDBCApp/src/in/ineuron/main/UpdateApp.java
@@ -0,0 +1,39 @@
+package in.ineuron.main;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class UpdateApp {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+	
+		
+		//Step-02 Establish the connection
+		String url ="jdbc:mysql:///octbatch";
+		String user ="root";
+		String password = "root";
+		Connection connection = DriverManager.getConnection(url, user, password);
+		System.out.println("Connection object created.....");
+		
+		//Step-03 create statement object and send the query
+		Statement statement = connection.createStatement();
+		System.out.println("Statement object created.....");
+		
+		//Step-04 execute the query and process the resultSet
+		String sqlUpdateQuery ="update student1 set sname='virat' where sid=12";
+		int rowCount = statement.executeUpdate(sqlUpdateQuery);
+		System.out.println("No. of row Affected : "+rowCount);
+		
+		
+		//Step-05 Handle SQLException if generated
+		// we have handle the exception by throws
+		
+		//Step-06 Close the resources
+		statement.close();
+		connection.close();
+		System.out.println("Closing the resources....");
+	}
+}
