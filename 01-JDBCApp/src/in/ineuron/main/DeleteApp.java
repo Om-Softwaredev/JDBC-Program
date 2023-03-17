c27f978a7adee2e83e28605ecbcc4c5ee83dd964
Omprakash <omprakash8578502@gmail.com>
2023-03-17 20:04:57 +0530

    DeleteApp is added

--- 01-JDBCApp/src/in/ineuron/main/DeleteApp.java
+++ 01-JDBCApp/src/in/ineuron/main/DeleteApp.java
@@ -2,21 +2,16 @@ package in.ineuron.main;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class SelectApp {
+public class DeleteApp {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
-		//Step-01 Load and register the Driver
-//		Class.forName("com.mysql.cj.jdbc.Driver");
-//		System.out.println("Loading the Driver....");
-		// from jdbc 4.X autoloading features enabled
+	
 		
 		//Step-02 Establish the connection
-//		String url ="jdbc:mysql://localhost:3306/octbatch";
 		String url ="jdbc:mysql:///octbatch";
 		String user ="root";
 		String password = "root";
@@ -28,28 +23,17 @@ public class SelectApp {
 		System.out.println("Statement object created.....");
 		
 		//Step-04 execute the query and process the resultSet
-		String sqlSelectQuery ="select sid,sname,sage,saddress from student1";
-		ResultSet resultSet = statement.executeQuery(sqlSelectQuery);
-		System.out.println("ResultSet object created.....");
+		String sqlDeleteQuery ="delete from student1 where sid =2";
+		int rowCount = statement.executeUpdate(sqlDeleteQuery);
+		System.out.println("No. of row Affected : "+rowCount);
 		
-		System.out.println("SID\tSNAME\tSAGE\tSADDRESS");
-		while (resultSet.next()) {
-			int sid = resultSet.getInt(1);
-			String sname=resultSet.getString(2);
-			int sage = resultSet.getInt(3);
-			String saddress=resultSet.getString(4);
-			System.out.println(sid+"\t"+sname+"\t"+sage+"\t"+saddress);
-		}
 		
 		//Step-05 Handle SQLException if generated
 		// we have handle the exception by throws
 		
 		//Step-06 Close the resources
-		resultSet.close();
 		statement.close();
 		connection.close();
 		System.out.println("Closing the resources....");
-		
 	}
-
 }
